06602f7da02146e2f2e5218444e080f3cff45f42
prasad123456789 <varaprasad_c@persistent.com>
2022-04-29 18:16:47 +0530

    added helloworld Programms

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+class Add
+{
+	public static void main(String []args)
+	{
+		int c=20;
+		int a=10;
+		int b=3;
+		int d=23;
+		int e=3;
+		System.out.println("Hello java");
+	}
+}
