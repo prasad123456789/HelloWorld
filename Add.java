fa84242b47205493c588f7ae546133ba8f03f469
prasad123456789 <varaprasad_c@persistent.com>
2022-05-02 09:11:33 +0530

    File modified

--- Add.java
+++ Add.java
@@ -1,13 +1,13 @@
 import java.lang.*;
-class Add
-{
-	public static void main(String []args)
-	{
-		int c=20;
-		int a=10;
-		int b=3;
-		int d=23;
-		int e=3;
+
+class Add {
+	public static void main(String[] args) {
+		int c = 20;
+		int a = 10;
+		int b = 3;
+		int d = 23;
+		int e = 3;
 		System.out.println("Hello java");
+		System.out.println(a + b);
 	}
 }
