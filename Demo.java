8d77f858f23a79b135fc0aa0db3fcf94465288c6
prasad123456789 <varaprasad_c@persistent.com>
2022-05-02 13:53:40 +0530

    Demo class added

---
+++ Demo.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+class Demo {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 0; i < 10; i++) {
+            System.out.println("Hello java " + i);
+        }
+    }
+}
